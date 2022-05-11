739dad3878f0aea403a6b6d3fe69f84a69cf6bf2
Jeffrey Chau <jeffreyd.chau@gmail.com>
2022-05-10 17:42:11 -0700

    coverage fix

--- app/src/androidTest/java/com/example/my_money_app/MainActivityTest.java
+++ app/src/androidTest/java/com/example/my_money_app/MainActivityTest.java
@@ -1,14 +1,25 @@
 package com.example.my_money_app;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import android.widget.DatePicker;
 
+import androidx.test.espresso.contrib.PickerActions;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.hamcrest.Matchers;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 public class MainActivityTest {
     @Rule
