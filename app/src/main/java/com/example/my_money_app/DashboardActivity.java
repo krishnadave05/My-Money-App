89e24077b05121fc2668a6342311bff73809a8af
alasali1 <alasali96@gmail.com>
2022-06-12 02:21:40 -0700

    Results Activity java program works

--- app/src/main/java/com/example/my_money_app/DashboardActivity.java
+++ app/src/main/java/com/example/my_money_app/DashboardActivity.java
@@ -2,13 +2,22 @@ package com.example.my_money_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class DashboardActivity extends AppCompatActivity {
-
+Button resultsBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        resultsBtn = findViewById(R.id.results_button);
+
+        //onClick for results button
+        resultsBtn.setOnClickListener(v -> {
+                Intent i = new Intent(DashboardActivity.this, ResultActivity.class);
+                startActivity(i);
+        });
     }
 }
