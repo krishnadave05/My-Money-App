04885428b738477410ada9066e4700b719085bd9
DipendraShiwakoti <dipendra.shiwakoti@go.shoreline.edu>
2022-06-13 23:33:48 -0700

    Decorated the input UI

--- app/src/main/java/com/example/my_money_app/Input.java
+++ app/src/main/java/com/example/my_money_app/Input.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,7 +27,6 @@ Button backButton;
         //instantiate elements
         hoursWorked = findViewById(R.id.hours_worked_input);
         hoursStudied = findViewById(R.id.hours_studied_input);
-        moneyMade = findViewById(R.id.MoneyMadeField);
         expenses = findViewById(R.id.ExpensesField);
         addBtn = findViewById(R.id.Add);
         resultButton = findViewById(R.id.results_button);
@@ -46,39 +46,61 @@ Button backButton;
         //make onclick with bundle to send data to result activity
         addBtn.setOnClickListener(v -> {
             //intent to go to result activity
-            Intent i = new Intent(Input.this, DashboardActivity.class);
 
-            //create bundle
-            Bundle bundle = new Bundle();
+                Intent i = new Intent(Input.this, DashboardActivity.class);
+                i.putExtra("message", "Login Screen");
+                startActivity(i);
 
-            //convert input to strings
-            String worked = hoursWorked.getText().toString();
-            String studied = hoursStudied.getText().toString();
-            String expen = expenses.getText().toString();
 
-            //put data in bundle
-            bundle.putString("work",worked);
-            bundle.putString("study",studied);
-            bundle.putString("expense",expen);
+                //create bundle
+                Bundle bundle = new Bundle();
 
-            i.putExtras(bundle);
-            startActivity(i);
+                //convert input to strings
+                String worked = hoursWorked.getText().toString();
+                String studied = hoursStudied.getText().toString();
+                String expen = expenses.getText().toString();
+
+                //put data in bundle
+                bundle.putString("work", worked);
+                bundle.putString("study", studied);
+                bundle.putString("expense", expen);
+
+                i.putExtras(bundle);
+                startActivity(i);
+
         });
     }
+//    boolean validateData() {
+//        boolean flag = true;
+//       // mNumber.getText().toString().length()>0
+//        if (hoursStudied.getText().toString().length()>0){
+//       // if (isEmpty(hoursStudied)) {
+//            hoursStudied.setError("Password can't be empty!");
+//            flag = false;
+//        }
+//        return flag;
+//    }
+//    boolean isEmpty(EditText text) {
+//        CharSequence str = text.getText().toString();
+//        return TextUtils.isEmpty(str);
+//    }
 
 
 
-public void onSubmit(View view){
-    String worked = hoursWorked.getText().toString();
-    String studied = hoursStudied.getText().toString();
-    String expen = expenses.getText().toString();
 
-    if ( hoursWorked.equals("")|| hoursStudied.equals("") || expenses.equals("") ) {
-        // empty strings are not valid form input show a Toast to the user
-        Toast.makeText(getApplicationContext(),"complete all the necessary field", Toast.LENGTH_LONG).show();
-        return;
-    }
-}
+//public void onSubmit(View view){
+//    String worked = hoursWorked.getText().toString();
+//    String studied = hoursStudied.getText().toString();
+//    String expen = expenses.getText().toString();
+//
+//    if ( hoursWorked.equals("")|| hoursStudied.equals("") || expenses.equals("") ) {
+//        // empty strings are not valid form input show a Toast to the user
+//        Toast.makeText(getApplicationContext(),"complete all the necessary field", Toast.LENGTH_LONG).show();
+//        return;
+//    }
+//    Intent intent = new Intent(getApplicationContext(), DashboardActivity.class);
+//    startActivity(intent);
+//}
 
 
 }
