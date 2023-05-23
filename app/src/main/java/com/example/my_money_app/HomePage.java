5c2066c3f05c4dbf29b43d17531fd38fbb480e81
Jeffrey Chau <jeffreyd.chau@gmail.com>
2023-05-22 19:19:01 -0700

    Import fixed

--- app/src/main/java/com/example/my_money_app/HomePage.java
+++ app/src/main/java/com/example/my_money_app/HomePage.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
